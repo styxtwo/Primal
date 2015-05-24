21d3f02b40b44fec8cca49b88cbe165fe7095d82
Styxtwo <>
2015-05-24 22:03:44 +0200

    Added Abstract Component

---
+++ Primal/Main/Elements/AbstractComponent.cs
@@ -0,0 +1,12 @@
+﻿using Primal.API;
+
+namespace Primal {
+    /// <summary>
+    /// Abstract Implementation of the IComponent Interface.
+    /// </summary>
+    class AbstractComponent : IComponent {
+        public void Dispose() {
+            //Empty, to be overridden.
+        }
+    }
+}
