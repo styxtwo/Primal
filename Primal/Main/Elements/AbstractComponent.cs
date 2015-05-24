b31ba0da237eacc095fe80937a9207bb89b03a53
Styxtwo <>
2015-05-24 22:04:24 +0200

    Made Abstract component public

--- Primal/Main/Elements/AbstractComponent.cs
+++ Primal/Main/Elements/AbstractComponent.cs
@@ -4,7 +4,7 @@ namespace Primal {
     /// <summary>
     /// Abstract Implementation of the IComponent Interface.
     /// </summary>
-    class AbstractComponent : IComponent {
+    public class AbstractComponent : IComponent {
         public void Dispose() {
             //Empty, to be overridden.
         }
